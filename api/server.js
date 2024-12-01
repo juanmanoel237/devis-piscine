72d63a098e8dc945339c423b166de22c927309fa
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-12-01 13:22:02 +0100

    Création du server

---
+++ api/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const User = require('./models/User'); // Import du modèle User
+const authRoutes = require('./routes/authRoutes');
+const devisRoutes = require('./routes/devisRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connecté'))
+  .catch(err => console.log(err));
+
+// Fonction pour créer un utilisateur admin si nécessaire
+const createAdmin = async () => {
+  const adminExist = await User.findOne({ email: process.env.ADMIN_EMAIL });
+
+  if (!adminExist) {
+    const admin = new User({
+      email: process.env.ADMIN_EMAIL,
+      password: process.env.ADMIN_PASSWORD, // Utilisation du mot de passe hardcodé
+      role: 'admin'
+    });
+
+    try {
+      await admin.save();
+      console.log('Administrateur créé avec succès');
+    } catch (error) {
+      console.error('Erreur lors de la création de l\'administrateur :', error);
+    }
+  }
+};
+
+createAdmin();
+
+app.use('/api/auth', authRoutes);
+app.use('/api/devis', devisRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
